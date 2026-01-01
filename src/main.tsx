b633dd2c4a846ec5a44c95d68c4421a22ba4babe
Warda-Samy <wardasamy1996@gmail.com>
2026-01-01 22:43:26 +0200

    fix i18n import

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
-import "./i18n";
 
 import App from "./App";
 import NavBar from "./navbear/Navbar";
